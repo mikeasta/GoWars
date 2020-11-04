c127654196737349c458b07a43f2dde3bb974599
Mikhail Astashonak <thenaitrise@gmail.com>
2020-11-04 22:09:43 +0300

    DNA/TwoNumbers added

---
+++ TwoNumbers/TwoNumbers.go
@@ -0,0 +1,36 @@
+// Write a function that takes an array of numbers (integers for the tests) and 
+// a target number. It should find two different items in the array that, when added together, 
+// give the target value. 
+// The indices of these items should then be returned in a tuple like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; 
+// any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, 
+// and all of the items will be numbers; target will always be the sum of two 
+// different items from that array).
+
+// Based on: http://oj.leetcode.com/problems/two-sum/
+// twoSum [1, 2, 3] 4 === (0, 2)
+
+package main
+import "fmt"
+
+var i, j int 
+func twoNumbers(slice []int, target int) [2]int {
+	i = 0
+	for ; i < len(slice); i++{
+		j = i + 1
+		for ;j < len(slice); j++ {
+			if (target == (slice[i] + slice[j])) {
+				return [2]int{i, j}
+			}
+		}
+	}
+	return [2]int{i, j}
+}
+
+func main() {
+	arr := []int{1, 2, 3}
+	fmt.Println(twoNumbers(arr, 4))
+}
